c2669b9573f753720be0756f4318a75512f77834
Peggycarter28 <159530338+Peggycarter28@users.noreply.github.com>
2024-08-19 10:40:23 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const toggle = document.getElementById('toggle')
+const transform = document.getElementById('transform')
+
+
+toggle.addEventListener('click', ()=>{
+main.style.background = 'black';
+})
+
+transform.addEventListener("click", ()=>{
+  main.style.background = 'white';  
+})
+
+
+
+
+
+
+
+
